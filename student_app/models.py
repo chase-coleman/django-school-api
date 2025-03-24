ffd9f06d9cc701c6f5f280499a35cab30b640fba
YOUR_NAME <YOUR_EMAIL>
2025-03-24 15:42:10 -0500

    Added model methods to reassign the student's locker, and their 'good_student' status

--- student_app/models.py
+++ student_app/models.py
@@ -9,10 +9,17 @@ class Student(models.Model):
   name = models.CharField(max_length=75, blank=False)
   student_email = models.EmailField(unique=True, null=False)
   personal_email = models.EmailField(unique=True, null=False)
-  locker_number = models.IntegerField(validators=[MaxValueValidator(500), MinValueValidator(1)])
-  locker_combination = models.CharField(max_length=8)
+  locker_number = models.IntegerField(unique=True,validators=[MaxValueValidator(500), MinValueValidator(1)], default=110)
+  locker_combination = models.CharField(max_length=8, default="12-12-12")
   good_student = models.BooleanField(null=False, default=False)
 
   def __repr__(self):
-    return f"{self.id}, {self.name}, {self.student_email}, {self.personal_email}, {self.locker_number}, {self.locker_combination}, {self.good_student}"
+    return f"{self.name} - {self.student_email} - {self.locker_number}"
   
+  def locker_reassignment(self, new_locker_num):
+    self.locker_number = new_locker_num 
+    self.save()
+
+  def student_status(self, new_student_rating):
+    self.good_student = new_student_rating
+    self.save()
