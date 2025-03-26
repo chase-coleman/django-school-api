ac8df0bcb28126f2615f949be097f5464c732005
YOUR_NAME <YOUR_EMAIL>
2025-03-25 20:43:44 -0500

    Added serializers

--- student_app/models.py
+++ student_app/models.py
@@ -9,13 +9,15 @@ class Student(models.Model):
     locker_number = models.IntegerField(default=110, unique= True, validators=[MinValueValidator(1), MaxValueValidator(200)])
     locker_combination = models.CharField(max_length=20, default = "12-12-12", blank=True, validators=[RegexValidator(r'^\d{2}-\d{2}-\d{2}$', message= 'Combination must be in the format "12-12-12"')] )
     good_student= models.BooleanField(default=True)
+    
     def __str__(self):
         return f"{self.name} - {self.student_email} - {self.locker_number}"
+    
     def locker_reassignment(self, new_locker_number):
         self.locker_number = new_locker_number
         self.save()
         return f"Student: {self.student.name}'s locker has been changed to {self.locker_number}"
+    
     def student_status(self, is_good_student):
         self.good_student = is_good_student
         self.save()
-        
