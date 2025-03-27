3bf9aa0e0c5e6b44f72385d36b6f2692e4fb79a6
YOUR_NAME <YOUR_EMAIL>
2025-03-27 08:40:04 -0500

    Added serializers for grade and subject. need to fix Grade class throwing an issue when doing .data with the serialized data in the shell

--- student_app/models.py
+++ student_app/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
-from django.contrib.postgres.fields import ArrayField
 import re
+
+
+# create subject class here
+class Subject(models.Model):
+    subject_name = models.CharField(unique=True, default="Unknown", null=False)
+    professor = models.CharField(unique=False, default="Mr. Cahan", null=False)
+    # the related_name="students" in the Student class will create an attribute of Subject that contains all the students in that subject
+
 
 class Student(models.Model):
     name = models.CharField(max_length=100, validators=[RegexValidator(r'^[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+$', message= 'Name must be in the format "First Middle Initial. Last"')])
@@ -10,14 +17,7 @@ class Student(models.Model):
     locker_number = models.IntegerField(default=110, unique= True, validators=[MinValueValidator(1), MaxValueValidator(200)])
     locker_combination = models.CharField(max_length=20, default = "12-12-12", blank=True, validators=[RegexValidator(r'^\d{2}-\d{2}-\d{2}$', message= 'Combination must be in the format "12-12-12"')] )
     good_student= models.BooleanField(default=True)
-    subjects = ArrayField(
-        models.IntegerField(validators=[
-            MinValueValidator(1),
-            MaxValueValidator(8)
-        ]),
-        blank=True,
-        default=list
-        )
+    subjects = models.ManyToManyField(Subject, related_name="students")
 
     def __str__(self):
         return f"{self.name} - {self.student_email} - {self.locker_number}"
@@ -32,19 +32,9 @@ class Student(models.Model):
         self.good_student = is_good_student
         self.full_clean()
         self.save()
-
-    def add_subject(self, subject_id):
-        if len(self.subjects) == 8:
-            raise Exception("This student's class schedule is full!")
-        else:
-            self.subjects.append(subject_id)
-            self.full_clean()
-            self.save()
-
-    def remove_subject(self, subject_id):
-        if not len(self.subjects): 
-            raise Exception("This students class schedule is empty!")
-        else:
-            self.subjects.remove(subject_id)
-            self.full_clean()
-            self.save()
+e
+# create grade class here 
+class Grade(models.Model):
+    grade = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="grades")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="grades")
--- student_app/serializers.py
+++ student_app/serializers.py
@@ -1,14 +1,28 @@
-from rest_framework import serializers 
-from .models import Student 
+from rest_framework.serializers import ModelSerializer 
+from .models import Student, Subject, Grade 
 
-class StudentSerializer(serializers.ModelSerializer):
+class StudentSerializer(ModelSerializer):
   class Meta:
     model = Student
     fields = ['name', 'student_email','locker_number']
 
-class StudentAllSerializer(serializers.ModelSerializer):
+class StudentAllSerializer(ModelSerializer):
   class Meta:
     model = Student
     fields = ['name', 'student_email', 'personal_email', 
             'locker_number', 'locker_combination', 'good_student']
 
+class SubjectSerializer(ModelSerializer):
+  class Meta:
+    model = Subject 
+    fields = ['subject_name', 'professor', 'students']
+  
+class SubjectAllSerializer(ModelSerializer):
+  class Meta:
+    model = Subject
+    fields = ['subject_name', 'professor', 'students']
+  
+class GradeSerializer(ModelSerializer):
+  class Meta:
+    model = Grade 
+    fields = ['grade', 'a_subject', 'student']
